2387e12ea261d441249ebcfddd3ab7c018eeffa3
Shaun Stephenson <shaunfitzgarald@gmail.com>
2023-10-17 11:51:49 -0700

    Finished MVP for api-endpoints

--- spec/requests/plants_spec.rb
+++ spec/requests/plants_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Plants", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of plants" do
+      Plant.create(
+        name: 'Prayer Plant',
+        age: 21,
+        enjoys: 'Enjoys being in direct sunlight between 60 and 72 degrees',
+        image: 'https://images.unsplash.com/photo-1637967886160-fd78dc3ce3f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByYXllciUyMHBsYW50c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60'
+      )
+      get '/plants'
+
+      plant = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(plant.length).to eq 1
+    end
+  end
+  describe "POST /create" do
+    it "creates a plant" do
+      plant_params = {
+        plant: {
+          name: 'Prayer Plant',
+        age: 21,
+        enjoys: 'Enjoys being in direct sunlight between 60 and 72 degrees',
+        image: 'https://images.unsplash.com/photo-1637967886160-fd78dc3ce3f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByYXllciUyMHBsYW50c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60'
+        }
+      }
+  
+      post '/plants', params: plant_params
+  
+      expect(response).to have_http_status(200)
+  
+      plant = Plant.first
+  
+      expect(plant.name).to eq 'Prayer Plant'
+    end
   end
 end
